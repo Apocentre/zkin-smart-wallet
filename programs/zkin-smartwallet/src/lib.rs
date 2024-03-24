9001f4c1b94342fb7ff1d7954e5069d21ebd9085
ppoliani <ppoliani@gmail.com>
2024-03-24 12:13:00 +0200

    feat: add init_zkp to the entrypoint

--- programs/zkin-smartwallet/src/lib.rs
+++ programs/zkin-smartwallet/src/lib.rs
@@ -5,7 +5,9 @@ mod processors;
 mod program_error;
 
 use anchor_lang::prelude::*;
-use crate::instructions::create_wallet::*;
+use crate::instructions::{
+  create_wallet::*, init_zkp::*,
+};
 
 declare_id!("zkinKSHW3PijK2ZyRDUSXe8m2UKnNjJPvnv2NeZUvHy");
 
@@ -36,6 +38,44 @@ pub mod zkin_smartwallet {
     public_inputs_vec: [u8; 308],
   ) -> Result<()> {
     processors::create_wallet::exec(
+      ctx,
+      proof_a,
+      proof_b,
+      proof_c,
+      public_inputs_vec,
+    )
+  }
+
+  /// Currently the Solana runtime has heap size limit of 32Kb per transaction. Our public inputs have a length
+  /// of 244 bytes which are converted into a Circo Field and each byte becomes [u8; 32]. The Groth16 verifier
+  /// use `alt_bn128`` to verify the ZKP. That library uses vectors and allocates more that 32 KB of heap memory.
+  /// More on this here https://github.com/anza-xyz/agave/issues/356
+  /// To circumvent this, we split the verification process into pre-processing. Each pre-process works on a section
+  /// of the public inputs (big enough to keep the heap size within the limits). The first step to run the pre-process
+  /// is to call this instruction which will store the ZKP and the subsequent instruction will use the data store in
+  /// this ix.
+  ///
+  /// # Arguments
+  /// 
+  /// * `ctx` - The Anchor context holding the accounts
+  /// * `wallet_address` - This is a deterministic address which is `address = H(sub|iss|aud|salt)` where H = Poseidon
+  /// It's a hex encoded bytes value. It should be part of the public_inputs_vec as well. But we pass it one more time as
+  /// a separate param so we can easily create the wallet PDA which the wallet_address is the seed of.
+  /// * `proof_a` - Part of the ZKP
+  /// * `proof_b` - Part of the ZKP
+  /// * `proof_c` - Part of the ZKP
+  /// * `public_inputs_vec` - All public inputs to the circuit. The len is calculated as so:
+  /// iss_out + aud_out + nonce_out + exp_out + wallet_address + rsa_modulo = 78 + 78 + 78 + 10 + 32 + 32
+  /// Note the wallet address and the rsa_modulo which are hex encoded values
+  pub fn init_zkp(
+    ctx: Context<InitZkp>,
+    _wallet_address: [u8; 32],
+    proof_a: [u8; 64],
+    proof_b: [u8; 128],
+    proof_c: [u8; 64],
+    public_inputs_vec: [u8; 308],
+  ) -> Result<()> {
+    processors::init_zkp::exec(
       ctx,
       proof_a,
       proof_b,
